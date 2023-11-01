55fd3879d7aa98b5d3a2bbc635238d326cdc94da
newDINO <new_dino@126.com>
2023-11-01 23:12:24 +0800

    add softkeyboard entry

--- demo/main.ts
+++ demo/main.ts
@@ -1,10 +1,14 @@
 import { MathIO } from "../target/mathio.js";
 import { SoftKeyboard } from "../target/soft_keyboard.js"
 
+let container = document.createElement('div');
+container.classList.add('container');
+document.body.appendChild(container);
+
 let mathio = new MathIO()
-document.body.appendChild(mathio.container);
+container.appendChild(mathio.container);
 mathio.container.setAttribute('tabindex', '0');
-mathio.container.classList.add('math_container');
+mathio.container.classList.add('math_container')
 mathio.container.addEventListener('pointerdown', (e) => {
     mathio.on_pointerdown(e);
 })
@@ -15,7 +19,24 @@ mathio.container.addEventListener('keydown', (e) => {
     mathio.on_key(e.key);
 })
 
+let under_bar = document.createElement('div')
+under_bar.classList.add("bar")
+container.appendChild(under_bar);
+let keyboard_button = document.createElement('div');
+keyboard_button.style.marginLeft = 'auto'
+under_bar.appendChild(keyboard_button)
+keyboard_button.classList.add('button')
+keyboard_button.style.backgroundImage = 'url("/demo/res/keyboard.svg")'
 let soft_keyboard = new SoftKeyboard((key) => {
     mathio.on_key(key)
 });
 soft_keyboard.container.classList.add('keyboard');
+document.body.appendChild(soft_keyboard.container);
+soft_keyboard.container.style.display = 'none'
+keyboard_button.addEventListener('pointerdown', () => {
+    if(soft_keyboard.container.style.display != '') {
+        soft_keyboard.container.style.display = ''
+    } else {
+        soft_keyboard.container.style.display = 'none'
+    }
+})
